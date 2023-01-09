4c94e8a4b40c0dd0c8a29984d851978de7337c01
Shrey <shreyparikh1002@gmail.com>
2023-01-09 21:13:43 +0530

    functional backend

--- app/src/main/java/com/example/scooby/FloatingService.java
+++ app/src/main/java/com/example/scooby/FloatingService.java
@@ -1,5 +1,7 @@
 package com.example.scooby;
 
+import static android.content.ContentValues.TAG;
+
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
@@ -10,6 +12,7 @@ import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.Build;
 import android.os.IBinder;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,23 +26,35 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FloatingService extends Service {
     private WindowManager wm;
     private LinearLayout ll;
     ImageView close;
-    Button emergency;
+    Button emergency,submit;
     View viewRoot;
-    EditText task1,task2;
-
+    EditText task1,task2,task3,task4;
+    int cnt=0;
     String[] courses = { "","Morning", "DSA",
             "Friends", "Wasted","Food"
              };
 //    ..................................................................................
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 //    planned structure dates folder -> date -> time slots doc -> (activity ,time ,tag)
 //    ..................................................................................
     public FloatingService() {
@@ -50,6 +65,14 @@ public class FloatingService extends Service {
         return null;
     }
     @Override
+
+
+
+
+
+
+
+
     public void onCreate() {
         super.onCreate();
         wm =(WindowManager ) getSystemService(WINDOW_SERVICE);
@@ -84,11 +107,13 @@ public class FloatingService extends Service {
         WindowManager.LayoutParams parameters=new WindowManager.LayoutParams(900, ViewGroup.LayoutParams.WRAP_CONTENT,LAYOUT_FLAG,WindowManager.LayoutParams.FLAG_BLUR_BEHIND, PixelFormat.TRANSLUCENT);
         close = viewRoot.findViewById(R.id.window_close);
         emergency = viewRoot.findViewById(R.id.emergency);
+        submit = viewRoot.findViewById(R.id.submit);
         task1 =viewRoot.findViewById(R.id.task1);
         task2 =viewRoot.findViewById(R.id.task2);
 
         close.setOnClickListener(view -> stopService());
         emergency.setOnClickListener(view -> stop());
+        submit.setOnClickListener(view -> save());
 
         parameters.x=0;
         parameters.y=0;
@@ -100,6 +125,11 @@ public class FloatingService extends Service {
         Spinner spin2 = viewRoot.findViewById(R.id.spinner2);
         Spinner spin3 = viewRoot.findViewById(R.id.spinner3);
         Spinner spin4 = viewRoot.findViewById(R.id.spinner4);
+
+        task1 = viewRoot.findViewById(R.id.task1);
+        task2 = viewRoot.findViewById(R.id.task2);
+        task3 = viewRoot.findViewById(R.id.task3);
+        task4 = viewRoot.findViewById(R.id.task4);
 //        spin.setOnItemSelectedListener(this);
 
         // Create the instance of ArrayAdapter
@@ -124,6 +154,38 @@ public class FloatingService extends Service {
         spin4.setAdapter(ad);
 
     }
+
+    private void save() {
+        cnt++;
+        Map<String, Object> tasks = new HashMap<>();
+        tasks.put("task1", task1.getText().toString());
+        tasks.put("task2", task2.getText().toString());
+        tasks.put("task3", cnt);
+
+
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+        String strDate = dateFormat.format(date);
+
+
+        db.collection("task").document(strDate)
+                .set(tasks)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
+    }
+
+
     private void stopService() {
 //        prevents closing tab until task added
         if(!task1.getText().toString().isEmpty() && !task2.getText().toString().isEmpty()){
@@ -157,4 +219,3 @@ public class FloatingService extends Service {
 //drain more battery , wastes data so better to create documents inside documents
 //but u are billed according to queries , so if for fetching some data again and again you have to make 3 queries due to hierarchy that's expensive
 //queries find documents in a single collection
-//
