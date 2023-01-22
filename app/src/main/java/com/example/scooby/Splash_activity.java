b5ce846e3e18f67bdb1c5599d6342b20f6f3ab91
Shrey <shreyparikh1002@gmail.com>
2023-01-22 22:33:27 +0530

    splash screen

---
+++ app/src/main/java/com/example/scooby/Splash_activity.java
@@ -0,0 +1,26 @@
+package com.example.scooby;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent ihome=new Intent(Splash_activity.this,MainActivity.class);
+                startActivity(ihome);
+                finish();
+            }
+        },4000);
+
+    }
+}
